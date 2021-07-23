431d384443d90f847f7bf08b0fb4be45b7b54cab
mandarinka99 <alina.oksentiuk@gmail.com>
2021-07-23 23:02:42 +0300

    menu-template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './sass/main.scss';
+
+import './styles.css';
+import menu from './menu.json';
+import itemsTemplate from './templates/menu-template.hbs';
+
+const inputNode = document.querySelector('#theme-switch-toggle');
+
+document.body.classList.add('light-theme');
+
+inputNode.addEventListener('change', () => {
+document.body.classList.toggle('dark-theme');
+document.body.classList.toggle('light-theme');
+});
+
+
+const menuNode = document.querySelector('.js-menu');
+console.log(menuNode);
+
+const markup = itemsTemplate(menu);
+menuNode.innerHTML = markup;
+
