c13abc413bf257c6a88d88e3c4d7beb63047c884
mandarinka99 <alina.oksentiuk@gmail.com>
2021-07-25 17:18:17 +0300

    change-theme file

---
+++ src/change-theme.js
@@ -0,0 +1,25 @@
+const inputNode = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+let theme = localStorage.getItem('data-theme');
+if (theme === Theme.DARK) {
+  document.body.classList.add(Theme.DARK);
+  inputNode.checked = true;
+} else {
+  document.body.classList.add(Theme.LIGHT);
+}   
+
+inputNode.addEventListener('change', () => {
+  let theme = localStorage.getItem('data-theme');
+  if (theme === Theme.LIGHT) {
+    localStorage.setItem("data-theme", Theme.DARK)
+  } else {
+    localStorage.setItem("data-theme", Theme.LIGHT)
+  }   
+  document.body.classList.toggle(Theme.DARK);
+  document.body.classList.toggle(Theme.LIGHT);
+});
--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 
 import './styles.css';
+import './change-theme';
 import menu from './menu.json';
 import itemsTemplate from './templates/menu-template.hbs';
-
-const inputNode = document.querySelector('#theme-switch-toggle');
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-let theme = localStorage.getItem('data-theme');
-if (theme === Theme.DARK) {
-  document.body.classList.add(Theme.DARK);
-  inputNode.checked = true;
-} else {
-  document.body.classList.add(Theme.LIGHT);
-}   
-
-inputNode.addEventListener('change', () => {
-  let theme = localStorage.getItem('data-theme');
-  if (theme === Theme.LIGHT) {
-    localStorage.setItem("data-theme", Theme.DARK)
-  } else {
-    localStorage.setItem("data-theme", Theme.LIGHT)
-  }   
-  document.body.classList.toggle(Theme.DARK);
-  document.body.classList.toggle(Theme.LIGHT);
-});
 
 const menuNode = document.querySelector('.js-menu');
 
