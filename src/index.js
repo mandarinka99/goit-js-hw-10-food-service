6d8699a4dfaefc5867a92b300e9af7dc0ccb4977
mandarinka99 <alina.oksentiuk@gmail.com>
2021-07-25 15:25:56 +0300

    theme-switch

--- src/index.js
+++ src/index.js
@@ -5,13 +5,29 @@ import itemsTemplate from './templates/menu-template.hbs';
 
 const inputNode = document.querySelector('#theme-switch-toggle');
 
-document.body.classList.add('light-theme');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+let theme = localStorage.getItem('data-theme');
+if (theme === Theme.DARK) {
+  document.body.classList.add(Theme.DARK);
+  inputNode.checked = true;
+} else {
+  document.body.classList.add(Theme.LIGHT);
+}   
 
 inputNode.addEventListener('change', () => {
-document.body.classList.toggle('dark-theme');
-document.body.classList.toggle('light-theme');
+  let theme = localStorage.getItem('data-theme');
+  if (theme === Theme.LIGHT) {
+    localStorage.setItem("data-theme", Theme.DARK)
+  } else {
+    localStorage.setItem("data-theme", Theme.LIGHT)
+  }   
+  document.body.classList.toggle(Theme.DARK);
+  document.body.classList.toggle(Theme.LIGHT);
 });
-
 
 const menuNode = document.querySelector('.js-menu');
 console.log(menuNode);
